bcdb0618a0cf600f30104bb7869d9ad9781ecf11
jan d <d_jan@ymail.com>
2017-02-17 08:39:52 +0100

    First implementation sampling with replacement

--- src/sample.js
+++ src/sample.js
@@ -14,20 +14,38 @@ var shuffle = require('./shuffle');
  * @param {number} n count of how many elements to take
  * @param {Function} [randomSource=Math.random] an optional source of entropy
  * instead of Math.random
+ * @param {Boolean} if true, sampling with replacement will be used
  * @return {Array} subset of n elements in original array
  * @example
  * var values = [1, 2, 4, 5, 6, 7, 8, 9];
  * sample(values, 3); // returns 3 random values, like [2, 5, 8];
+
+
+
  */
 function sample/*:: <T> */(
     array /*: Array<T> */,
     n /*: number */,
-    randomSource /*: Function */) /*: Array<T> */ {
-    // shuffle the original array using a fisher-yates shuffle
-    var shuffled = shuffle(array, randomSource);
+    randomSource /*: Function */,
+    isWithReplacement /*: Boolean */) /*: Array<T> */ {
 
-    // and then return a subset of it - the first `n` elements.
-    return shuffled.slice(0, n);
+    var sampledValues = [];
+
+    if(isWithReplacement){
+        for(var i=0; i < n; i++){//do for as many samples element you need (n)
+            //generate random number that can be an index for array
+            var randomIndex = Math.floor(Math.random() * (array.length));
+            //add a random value (selected by randomIndex) to the sampledValues-Array
+            sampledValues.push(array[randomIndex]);
+        }
+    } else {
+        // shuffle the original array using a fisher-yates shuffle
+        var shuffled = shuffle(array, randomSource);
+        // and then select a subset of it - the first `n` elements.
+        sampledValues = shuffled.slice(0, n);
+    }
+
+    return sampledValues;
 }
 
 module.exports = sample;
